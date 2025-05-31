f0cae87c3276532a535dbfc56f7473d754354d20
Mo7nex <102780425+Mo7nex@users.noreply.github.com>
2025-05-31 16:26:53 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 # 训练参数官方详解链接：https://docs.ultralytics.com/modes/train/#resuming-interrupted-trainings:~:text=a%20training%20run.-,Train%20Settings,-The%20training%20settings
 
 if __name__ == '__main__':
-    model = YOLO('/home/ipmi/DeepLearning/models/ultralytics-20240822/ultralytics-main/ultralytics/cfg/models/yolov8-HFAM-LFAU-CSRN.yaml')
+    model = YOLO('ultralytics/cfg/models/yolov8-HFAM-LFAU-CSRN.yaml')
     # model.load('yolov8n.pt') # loading pretrain weights
     model.train(data='data/GTSDB_513.yaml',
                 cache=False,
@@ -20,6 +20,6 @@ if __name__ == '__main__':
                 # resume=True, 
                 # amp=False, # close amp
                 # fraction=0.2,
-                project='runs_light/GTSDB_513/original',
-                name='yolov8s-HFAM-LFAU-CSRN',
+                project='runs',
+                name='project_name',
                 )
