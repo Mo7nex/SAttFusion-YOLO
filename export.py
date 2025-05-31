680a39217ed7e99619c72a1c20ec9dc708b92b80
Mo7nex <102780425+Mo7nex@users.noreply.github.com>
2025-05-31 16:27:41 +0800

    Update export.py

--- export.py
+++ export.py
@@ -4,7 +4,6 @@ from ultralytics import YOLO
 
 # onnx onnxsim onnxruntime onnxruntime-gpu
 
-# 导出参数官方详解链接：https://docs.ultralytics.com/modes/export/#usage-examples
 
 if __name__ == '__main__':
     model = YOLO('yolov8n.pt')
